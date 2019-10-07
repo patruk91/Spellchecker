c4b29ce623d14dcaf5517499385e2c904a4ca5c8
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-10-07 10:15:19 +0200

    Add LousyStringHasher class

---
+++ Spellchecker/LousyStringHasher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spellchecker
+{
+    class LousyStringHasher : IStringHasher
+    {
+        public int Hash(string s)
+        {
+            int h = 0;
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                h += s[i];
+            }
+
+            return h;
+        }
+    }
+}
