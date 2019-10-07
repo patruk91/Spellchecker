b74e1bf467f51cdd46fd3eef19801d7e099c962f
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-10-07 10:07:24 +0200

    Add DegenerateStringHasher class

---
+++ Spellchecker/DegenerateStringHasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spellchecker
+{
+    class DegenerateStringHasher : IStringHasher
+    {
+        public int Hash(string s)
+        {
+            return 0;
+        }
+    }
+}
