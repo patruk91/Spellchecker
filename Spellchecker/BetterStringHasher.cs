fd6c0725c60193ad36d4ea1278d3ee51d59030be
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-10-07 10:11:59 +0200

    Add BetterStringHasher class

---
+++ Spellchecker/BetterStringHasher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spellchecker
+{
+    class BetterStringHasher : IStringHasher
+    {
+        public int Hash(string s)
+        {
+            int h = 0;
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                h *= 37;
+                h += s[i];
+            }
+
+            return h;
+        }
+    }
+}
